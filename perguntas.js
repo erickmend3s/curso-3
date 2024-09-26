2f083df4097b545b78e443252ada8df96fd6f31f
erickmend3s <143853475+erickmend3s@users.noreply.github.com>
2024-09-25 22:29:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futsal',
+    'Qual o melhor jogador da historia do futsal?',
+    ' Falcão!!!'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual a unica seleção penta campeã',
+    'Seleção brasileira'
+)
+
+criaCartao(
+    'Qual o melhor jeito de aprender a jogar',
+    'Apenas nascendo com talento?',
+    'Não, com bastante esforço e dedicação'
+)
+
+criaCartao(
+    'Um fato curioso',
+    'Qual o unico time que tem dois mundial e uma libertadores?',
+    'Corinthians'
+)
